84ef938ff8f56d3a5aa532c0476bf1ceb564aac0
olejodd10 <67640511+olejodd10@users.noreply.github.com>
2023-07-17 19:13:58 +0200

    Swapped buffer directions

--- lua/buffers.lua
+++ lua/buffers.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("", "<M-right>", vim.cmd.bnext) -- Consider just <right> and <left>
-vim.keymap.set("", "<M-left>", vim.cmd.bprevious)
+vim.keymap.set("", "<M-right>", vim.cmd.bprevious) -- Consider just <right> and <left>
+vim.keymap.set("", "<M-left>", vim.cmd.bnext)
 vim.keymap.set("", "<M-down>", vim.cmd.bdelete)
