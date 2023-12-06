cfce11698639f5156c05ae34ead58540a32c1ddf
Ole Johan <olejodd10@gmail.com>
2023-12-06 21:49:02 +0100

    Added <leader>D remap

--- lua/remap.lua
+++ lua/remap.lua
@@ -6,6 +6,7 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>D", "\"_D")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
