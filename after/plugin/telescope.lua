422a43ee254ccb995d013e66c6b16ec53ad80def
Ole Johan <olejodd10@gmail.com>
2026-03-12 22:42:41 +0100

    Base telescope file and grep searches in project root of current buffer
    Should be adjusted for other kinds of projects
    Will not work as intended if there is a CMakeLists.txt between the buffer and the project root

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,37 @@
+local util = require("lspconfig.util")
+
+local function project_root()
+  local bufname = vim.api.nvim_buf_get_name(0)
+
+  local root = util.root_pattern(
+    ".git",
+    "compile_commands.json",
+    "CMakeLists.txt"
+  )(bufname)
+
+  if root then
+    return root
+  end
+
+  return vim.uv.cwd()
+end
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local function project_find_files()
+  builtin.find_files({
+    cwd = project_root()
+  })
+end
+
+local function project_live_grep()
+  builtin.live_grep({
+    cwd = project_root()
+  })
+end
+
+vim.keymap.set('n', '<leader>ff', project_find_files, {})
+vim.keymap.set('n', '<leader>fg', project_live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fw', builtin.grep_string, {})
