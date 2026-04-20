98e3e6078fa39352f97122e713a451154e9619f2
Ole Johan Walderhaug <ole.walderhaug@heimdallpower.com>
2026-04-20 16:12:55 +0200

    Make ColorColumn visible

--- lua/config/citruszest.lua
+++ lua/config/citruszest.lua
@@ -1,4 +1,5 @@
 local background = "#121212"
+local subtle_gray = "#2a2a2a"
 local bright_black = "#767C77"
 local a_lighter_bg = bright_black
 local a_darker_fg_to_contrast_with_it = background
@@ -8,6 +9,7 @@ require("citruszest").setup({
     StatusLine = { fg = a_darker_fg_to_contrast_with_it, bg = a_lighter_bg },
     WinSeparator = { fg = a_lighter_bg, bg = a_darker_fg_to_contrast_with_it },
     NvimTreeWinSeparator = { fg = a_lighter_bg, bg = a_darker_fg_to_contrast_with_it },
+    ColorColumn = { bg = subtle_gray },
   },
 })
 vim.cmd("colorscheme citruszest")
