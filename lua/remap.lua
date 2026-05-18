3892ac38a27d3b1164c3b07c9f9635a22ed9a723
Ole Johan Walderhaug <ole.walderhaug@heimdallpower.com>
2026-05-18 09:26:24 +0200

    Add insert mode C-h remap
    This is what is sent when I press C-BS

--- lua/remap.lua
+++ lua/remap.lua
@@ -21,6 +21,7 @@ vim.keymap.set("i", "{<CR>", "{}<left><CR>")
 
 -- Intuitive insert mode stuff
 vim.keymap.set("i", "<C-BS>", "<C-w>")
+vim.keymap.set("i", "<C-H>", "<C-w>") -- <C-H> is what most terminals send for Ctrl+Backspace
 vim.keymap.set("i", "<C-DEL>", "<Esc>ldwi") -- Delete word behind the cursor
 vim.keymap.set("i", "<C-v>", "<C-r>+")
 vim.keymap.set("i", "<S-Tab>", "<C-d>")
