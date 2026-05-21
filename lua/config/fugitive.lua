94b74043f1b7cc075e4551bf80d9e3eed5c4f794
Ole Johan Walderhaug <ole.walderhaug@heimdallpower.com>
2026-05-21 15:17:49 +0200

    Improve fugitive remaps

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -1,3 +1,11 @@
+-- Prefer git aliases that are reusable outside nvim rather than cool remaps.
+-- Avoiding add and commit remaps to not mess things up with fast fingers.
+-- We're not in _that_ much of a rush.
+
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gc", ":Git ")
+vim.keymap.set("n", "<leader>gi", ":Git ")
+
 vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
+vim.keymap.set("n", "<leader>gv", ":Gvdiffsplit<CR>") -- Cool beans
+vim.keymap.set("n", "<leader>gl", ":Git log<CR>")
+vim.keymap.set("n", "<leader>gd", ":Git diff") -- No <CR> to leave access to aliases and args
