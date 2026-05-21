84395793464907c4aa7f1c970bbc8f1447297ac4
Ole Johan Walderhaug <Oolejodd10@gmail.com>
2026-05-22 00:31:10 +0200

    Cleanup colorscheme

--- lua/config/citruszest.lua
+++ lua/config/citruszest.lua
@@ -1,15 +1,20 @@
-local background = "#121212"
-local subtle_gray = "#2a2a2a"
-local bright_black = "#767C77"
-local a_lighter_bg = bright_black
-local a_darker_fg_to_contrast_with_it = background
+local palette = require("citruszest.palettes.colors")
+
+local black = palette.background
+local dark_gray = palette.visual
+local gray = palette.bright_black
+local light_gray = palette.white
 
 require("citruszest").setup({
   style = {
-    StatusLine = { fg = a_darker_fg_to_contrast_with_it, bg = a_lighter_bg },
-    WinSeparator = { fg = a_lighter_bg, bg = a_darker_fg_to_contrast_with_it },
-    NvimTreeWinSeparator = { fg = a_lighter_bg, bg = a_darker_fg_to_contrast_with_it },
-    ColorColumn = { bg = subtle_gray },
+    StatusLine = { fg = black, bg = light_gray },
+    StatusLineNC = { fg = black, bg = gray },
+
+    WinSeparator = { fg = light_gray, bg = black },
+    NvimTreeWinSeparator = { fg = light_gray, bg = black },
+
+    ColorColumn = { bg = dark_gray },
   },
 })
+
 vim.cmd("colorscheme citruszest")
