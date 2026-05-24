35221c03aacfc5f6cadd576a618f66ab60f655bc
Ole Johan Walderhaug <Oolejodd10@gmail.com>
2026-05-24 19:56:22 +0200

    Use vertical splits for Git commands

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -2,12 +2,12 @@
 -- Avoiding add and commit remaps to not mess things up with fast fingers.
 -- We're not in _that_ much of a rush.
 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-vim.keymap.set("n", "<leader>gi", ":Git ")
+vim.keymap.set("n", "<leader>gs", ":vert Git<CR>")
+vim.keymap.set("n", "<leader>gi", ":vert Git ")
 
 vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
 vim.keymap.set("n", "<leader>gv", ":Gvdiffsplit<CR>") -- Cool beans
-vim.keymap.set("n", "<leader>gl", ":Git log<CR>")
-vim.keymap.set("n", "<leader>gd", ":Git diff") -- No <CR> to leave access to aliases and args
+vim.keymap.set("n", "<leader>gl", ":vert Git log<CR>")
+vim.keymap.set("n", "<leader>gd", ":vert Git diff") -- No <CR> to leave access to aliases and args
 
 vim.keymap.set("n", "<leader>gx", ":GBrowse<CR>")
