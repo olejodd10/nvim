f7e833006fb0ebd5326e1688cba49edeef05bcf9
Ole Johan Walderhaug <olejodd10@gmail.com>
2026-09-21 23:02:57 +0200

    Refactor rwnu drawing

--- lua/rwnu/init.lua
+++ lua/rwnu/init.lua
@@ -9,8 +9,8 @@ local in_superscript = false
 
 local enabled = true
 
-local last_update_pos = nil
-local row_diff_limit = 2 -- The minimum row movement required for update
+local last_pos = nil
+local row_diff_limit = 2 -- The minimum row movement required for drawing overlay
 
 local winbar_enabled = true
 
@@ -35,7 +35,7 @@ local function clear(win_id)
         clear_winbar(win_id)
     end
 
-    last_update_pos = nil
+    last_pos = nil
 end
 
 local function window_is_floating(win_id)
@@ -51,24 +51,12 @@ local function clear_all()
   end
 end
 
-local function update_winbar(win_id)
+local function draw_winbar(win_id, number_line, cursor_display_start, cursor_display_width)
   local offset = vim.fn.getwininfo(win_id)[1].textoff
-
-  local row, col = unpack(vim.api.nvim_win_get_cursor(win_id))
-  local line = vim.api.nvim_buf_get_lines(
-    vim.api.nvim_win_get_buf(win_id),
-    row - 1,
-    row,
-    false
-  )[1] or ""
-
-  -- col is 0-indexed, cursor_char is 1-indexed
-  local cursor_char = utf8.byte_to_char_index(line, col)
 
   -- Note that the cursor number is positioned using the display column of the cursor,
   -- but shows the character index, which may be different (think tabs).
   local padding = string.rep(" ", offset)
-  local number_line, cursor_display_start, cursor_display_width = rwnu_line.make_number_line(line, cursor_char, false, false) -- TODO use super-/subscript in winbar? Do we ever want that?
   local win_width = vim.api.nvim_win_get_width(win_id)
   local truncated_number_line = utf8.truncate_at(number_line, win_width - offset + 1) -- Truncate because winbar does not handle line wrapping well
   if truncated_number_line ~= "" then
@@ -91,7 +79,7 @@ local function row_below_buffer(buf_id, row)
     return row >= buf_lines
 end
 
-local function update_overlay_row_inline(number_line_table, cursor_display_start, cursor_display_width, buf_id, overlay_row)
+local function draw_overlay_row_inline(number_line_table, cursor_display_start, cursor_display_width, buf_id, overlay_row)
     for k, v in pairs(number_line_table) do
         local char_style = style.get_overlay_style(cursor_display_start, cursor_display_width, k)
         local opts = {
@@ -104,7 +92,7 @@ local function update_overlay_row_inline(number_line_table, cursor_display_start
     end
 end
 
-local function update_overlay_row_eof(number_line_table, cursor_display_start, cursor_display_width, buf_id, overlay_row)
+local function draw_overlay_row_eof(number_line_table, cursor_display_start, cursor_display_width, buf_id, overlay_row)
     local number_line = rwnu_line.make_number_line_from_table(number_line_table)
     local virt_line = style.stylize_number_line_tuples(number_line, cursor_display_start, cursor_display_width)
     local opts = {
@@ -123,20 +111,35 @@ local function diff(a, b)
     end
 end
 
-local function overlay_can_be_skipped(win_id, row)
-    if not last_update_pos then
-        -- On the first drawing we rarely want a word motion - skip
-        return true
-    elseif last_update_pos.win_id ~= win_id then
+local function movement_qualifies_for_overlay(win_id, row)
+    if not last_pos then
+        -- On the initial movement we rarely want a word motion - skip
+        return false
+    elseif last_pos.win_id ~= win_id then
         -- When entering a window we rarely want a word motion - skip
-        return true
+        return false
     else
         -- We have moved within the window - check whether movement is significant
-        return diff(last_update_pos.row, row) < row_diff_limit
+        return diff(last_pos.row, row) >= row_diff_limit
     end
 end
 
-local function update_overlay(win_id)
+local function draw_overlay(buf_id, row, number_line_table, cursor_display_start, cursor_display_width)
+    local overlay_row = row - 1 + overlay_offset
+    if row_in_buffer(buf_id, overlay_row) then
+        draw_overlay_row_inline(number_line_table, cursor_display_start, cursor_display_width, buf_id, overlay_row)
+    elseif row_below_buffer(buf_id, overlay_row) then
+        local line_count = vim.api.nvim_buf_line_count(buf_id)
+        overlay_row = math.max(1, math.min(line_count, overlay_row)) -- overlay_row will be zero if buffer is empty
+        draw_overlay_row_eof(number_line_table, cursor_display_start, cursor_display_width, buf_id, overlay_row)
+    end
+end
+
+local function on_movement(win_id)
+    if window_is_floating(win_id) then
+        return
+    end
+
     local buf_id = vim.api.nvim_win_get_buf(win_id)
 
     local row, col = unpack(vim.api.nvim_win_get_cursor(win_id))
@@ -152,47 +155,32 @@ local function update_overlay(win_id)
 
     -- Note that the cursor number is positioned using the display column of the cursor,
     -- but shows the character index, which may be different (think tabs).
-    local number_line_table, cursor_display_start, cursor_display_width = rwnu_line.make_number_line_table(line, cursor_char, in_subscript, in_superscript)
 
     clear_overlay(buf_id)
 
-    if overlay_can_be_skipped(win_id, row) then
-        return
+    if overlay_enabled and movement_qualifies_for_overlay(win_id, row) then
+        local number_line_table, cursor_display_start, cursor_display_width = rwnu_line.make_number_line_table(line, cursor_char, in_subscript, in_superscript)
+        draw_overlay(buf_id, row, number_line_table, cursor_display_start, cursor_display_width)
     end
 
-    local overlay_row = row - 1 + overlay_offset
-    if row_in_buffer(buf_id, overlay_row) then
-        update_overlay_row_inline(number_line_table, cursor_display_start, cursor_display_width, buf_id, overlay_row)
-    elseif row_below_buffer(buf_id, overlay_row) then
-        local line_count = vim.api.nvim_buf_line_count(buf_id)
-        overlay_row = math.max(1, math.min(line_count, overlay_row)) -- overlay_row will be zero if buffer is empty
-        update_overlay_row_eof(number_line_table, cursor_display_start, cursor_display_width, buf_id, overlay_row)
-    end
-
-end
-
-local function update(win_id)
-    -- TODO reuse number line if both enabled
-    if overlay_enabled then
-        update_overlay(win_id)
-    end
     if winbar_enabled then
-        update_winbar(win_id)
+        -- A bit wasteful to not reuse the number line table here,
+        -- but we do it to avoid sub-/superscript in winbar
+        local number_line, cursor_display_start, cursor_display_width = rwnu_line.make_number_line(line, cursor_char, false, false)
+        draw_winbar(win_id, number_line, cursor_display_start, cursor_display_width)
     end
 
-    local row = vim.api.nvim_win_get_cursor(win_id)[1]
-    last_update_pos = {
+    last_pos = {
         row = row,
         win_id = win_id,
     }
 end
 
-local function update_all()
+local function draw_all()
   local win_ids = vim.api.nvim_list_wins()
   for _, win_id in ipairs(win_ids) do
-      if not window_is_floating(win_id) then
-          update(win_id)
-      end
+      -- Not ideal but quite close to what we want
+      on_movement(win_id)
   end
 end
 
@@ -221,14 +209,12 @@ function M.enable()
     group = group,
     callback = function()
         local current_win = vim.api.nvim_get_current_win()
-        if not window_is_floating(current_win) then
-            update(current_win)
-        end
+        on_movement(current_win)
     end,
   })
 
   enabled = true
-  update_all()
+  draw_all()
 end
 
 function M.disable()
